d484070a7b00ddfce8a791f1d5e094eb0aa420fc
Ankit <ankit@kashware.com>
2023-01-31 21:14:19 +0530

    firebase, theme class

---
+++ lib/theme/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+/*
+  This class contains all the colors used in the app.
+  Any future updates in the app theme can be changed by here.
+*/
+
+  static const Color primary = Color(0xffFFFFFF);
+  static const Color border = Color(0xffD8DCE8);
+
+  //----------------Text Colors-------------------------
+  static const Color light = Color(0xff757575);
+  static const Color bold = Color(0xff404553);
+}
